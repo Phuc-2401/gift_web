5151b739383959e6f9e4250b9df90a0ddf13ecba
Vu Thanh Thuong <thuong23042003@gmail.com>
2025-02-12 01:06:41 +0700

    fix api home page

--- src/views/auth/RegisterPage.vue
+++ src/views/auth/RegisterPage.vue
@@ -1,171 +1,213 @@
 <template>
-    <div class="register-page">
-        <div class="register-form">
-            <h1>Đăng Ký</h1>
-            <v-form @submit.prevent="register">
-                <div class="register-input">
-                    <v-text-field class="input" v-model="name" label="Tên" type="text" dense outline></v-text-field>
-                    <v-text-field class="input" v-model="email" label="Email" type="email" dense outline></v-text-field>
+  <div class="register-page">
+    <div class="register-form">
+      <h1>Đăng Ký</h1>
+      <v-form @submit.prevent="register">
+        <div class="register-input">
+          <v-text-field
+            class="input"
+            v-model="name"
+            label="Tên"
+            type="text"
+            dense
+            outline
+          ></v-text-field>
+          <v-text-field
+            class="input"
+            v-model="email"
+            label="Email"
+            type="email"
+            dense
+            outline
+          ></v-text-field>
 
-                    <v-text-field class="input" v-model="password" label="Mật khẩu" type="password" outline
-                        dense></v-text-field>
-                    <v-text-field class="input" v-model="repassword" label="Nhập lại mật khẩu" type="password" outline
-                        dense></v-text-field>
-                    <v-btn type="submit" class="register-button">Đăng ký</v-btn>
-                </div>
-                <div class="login-router">Bạn đã có tài khoản? <router-link
-                        style="color: #DD5D5D;text-decoration: none; " to="login">Đăng
-                        nhập
-                        ngay</router-link></div>
-            </v-form>
+          <v-text-field
+            class="input"
+            v-model="password"
+            label="Mật khẩu"
+            type="password"
+            outline
+            dense
+          ></v-text-field>
+          <v-text-field
+            class="input"
+            v-model="repassword"
+            label="Nhập lại mật khẩu"
+            type="password"
+            outline
+            dense
+          ></v-text-field>
+          <v-btn type="submit" class="register-button">Đăng ký</v-btn>
         </div>
-        <v-dialog v-model="showQrDialog" max-width="500px">
-            <v-card>
-                <v-card-title class="headline">Quét mã QR</v-card-title>
-                <v-card-text>
-                    <div class="qr-code">
-                        <img :src="qrImage" alt="QR Code" />
-                    </div>
-                    <div class="input-fields">
-                        <v-text-field
-                            v-model="codes"
-                            label="Mã số"
-                            type="text"
-                            maxlength="6"
-                            class="code-input"
-                        ></v-text-field>
-                    </div>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" @click="verifyCode">Xác nhận</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+        <div class="login-router">
+          Bạn đã có tài khoản?
+          <router-link style="color: #dd5d5d; text-decoration: none" to="login"
+            >Đăng nhập ngay</router-link
+          >
+        </div>
+      </v-form>
     </div>
+    <v-dialog v-model="showQrDialog" max-width="500px">
+      <v-card>
+        <v-card-title class="headline">Quét mã QR</v-card-title>
+        <v-card-text>
+          <div class="qr-code">
+            <img :src="qrImage" alt="QR Code" />
+          </div>
+          <div class="input-fields">
+            <v-text-field
+              v-model="codes"
+              label="Mã số"
+              type="text"
+              maxlength="6"
+              class="code-input"
+            ></v-text-field>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="verifyCode">Xác nhận</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-import { getAuthQr, register } from '@/api/auth_api';
-import QRCode from 'qrcode';
+import { getAuthQr, register } from "@/api/auth_api";
+import QRCode from "qrcode";
 export default {
-    name: 'RegisterPage',
-    data() {
-        return {
-            name:'',
-            email: '',
-            password: '',
-            repassword: '',
-            showQrDialog: false,
-            qrImage: '',
-            google2faSecret: '',
-            codes: ''
+  name: "RegisterPage",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      repassword: "",
+      showQrDialog: false,
+      qrImage: "",
+      codes: "",
+      google2fa_secret: "",
+    };
+  },
+  methods: {
+    async register() {
+      if (this.password !== this.repassword) {
+        this.$toast.error("Mật khẩu không khớp");
+        return;
+      }
+      try {
+        const response = await getAuthQr(this.name, this.email, this.password);
+        if (response.status === 200) {
+          this.google2fa_secret = response.google2fa_secret;
+          QRCode.toDataURL(response.qr_image, (err, url) => {
+            if (err) {
+              console.error(err);
+              this.$toast.error("Không thể tạo mã QR");
+            } else {
+              this.qrImage = url;
+              console.log(this.qrImage);
+              this.showQrDialog = true;
+            }
+          });
         }
-    }, methods: {
-        async register() {
-            console.log('gọi đến register');
-            if (this.password !== this.repassword) {
-                this.$toast.error('Mật khẩu không khớp');
-                return;
-            }
-            try {
-                const response = await getAuthQr(this.name,this.email, this.password);
-                if (response.status === 200) {
-                    this.google2faSecret = response.google2fa_secret;
-                    QRCode.toDataURL(response.qr_image, (err, url) => {
-                        if (err) {
-                            console.error(err);
-                            this.$toast.error('Không thể tạo mã QR');
-                        } else {
-                            this.qrImage = url;
-                            console.log(this.qrImage);
-                            this.showQrDialog = true;
-                        }
-                    });
-                }
-            } catch (err) {
-                this.$toast.error('Đăng ký thất bại');
-            }
-        },
-        async verifyCode() {
-            try {
-                const response = await register(this.name,this.email, this.password, this.codes);
-                if (response.status === 200) {
-                    this.$toast.success('Đăng ký thành công');
-                    this.showQrDialog = false;
-                }
-            } catch (err) {
-                this.$toast.error('Mã xác thực không chính xác');
-            }
+      } catch (err) {
+        this.$toast.error(err.response.data.message);
+      }
+    },
+    async verifyCode() {
+      var data = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        codes: this.codes,
+      };
+      console.log(data);
+      try {
+        const response = await register(
+          this.name,
+          this.email,
+          this.password,
+          this.codes,
+          this.google2fa_secret
+        );
+        if (response.status === 200) {
+          this.$toast.success("Đăng ký thành công");
+          this.showQrDialog = false;
+          this.$router.push("/login");
         }
-    }
-}
+      } catch (err) {
+        this.$toast.error(err.response.data.message);
+      }
+    },
+  },
+};
 </script>
 <style scoped>
 .register-page {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #f5f5f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
 }
 
 .register-form {
-    width: 400px;
-    padding: 20px;
-    background-color: #fff;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  width: 400px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 .register-input {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .input {
-    margin-bottom: 10px;
-    border-radius: 12px;
+  margin-bottom: 10px;
+  border-radius: 12px;
 }
 
 h1 {
-    text-align: center;
-    margin-bottom: 10px;
+  text-align: center;
+  margin-bottom: 10px;
 }
 
 .login-router {
-    text-align: right;
-    margin-top: 10px;
-    color: rgb(94, 94, 94);
+  text-align: right;
+  margin-top: 10px;
+  color: rgb(94, 94, 94);
 }
 
 .register-button {
-    box-shadow: none;
-    background-color: #DD5D5D !important;
-    color: #fff !important;
-    font-weight: 500;
+  box-shadow: none;
+  background-color: #dd5d5d !important;
+  color: #fff !important;
+  font-weight: 500;
 }
 
 .register-button:hover {
-    box-shadow: none;
-    background-color: #fff !important;
-    color: #DD5D5D !important;
-    border: #DD5D5D 1px solid;
-    font-weight: 500;
-    transition: background-color 0.5s ease, color 0.5s ease;
+  box-shadow: none;
+  background-color: #fff !important;
+  color: #dd5d5d !important;
+  border: #dd5d5d 1px solid;
+  font-weight: 500;
+  transition: background-color 0.5s ease, color 0.5s ease;
 }
 .qr-code {
-    text-align: center;
-    margin-bottom: 20px;
+  text-align: center;
+  margin-bottom: 20px;
 }
 
 .input-fields {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 .code-input {
-    width: 40px;
-    text-align: center;
+  width: 40px;
+  text-align: center;
 }
 </style>
--- src/views/user/Home.vue
+++ src/views/user/Home.vue
@@ -1,109 +1,129 @@
 <template>
-    <div class="home-page">
-        <h1 class="page-title">Danh Sách Sản Phẩm</h1>
-        <div class="images">
-            <div v-for="product in products" :key="product.id" class="product-card">
-                <img :src="product.image" alt="product image" />
-                <div class="product-info">
-                    <h3>{{ product.name }}</h3>
-                    <p>{{ product.original_price.toLocaleString() }} VND</p>
-                </div>
-            </div>
+  <div class="home-page">
+    <h1 class="page-title">Danh Sách Sản Phẩm</h1>
+    <div class="images">
+      <div v-for="product in products" :key="product.id" class="product-card">
+        <img :src="product.image" alt="product image" />
+        <div class="product-info">
+          <h3>{{ product.name }}</h3>
+          <p>{{ product.original_price.toLocaleString() }} VND</p>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import { getHome } from "../../api/home_api";
 export default {
-    name: "HomePage",
-    data() {
-        return {
-            products: [
-                {
-                    id: 1,
-                    name: "Quà năm mới",
-                    image: "https://www.angroos.com/wp-content/uploads/2024/12/realistic-New-Year-celebration-scene-featuring-a-luxurious-indoor.jpg",
-                    original_price: 100000,
-                },
-                {
-                    id: 2,
-                    name: "Quà Valentine",
-                    image: "https://img.freepik.com/premium-photo/happy-valentine-day-gift-2023_781361-70.jpg",
-                    original_price: 200000,
-                },
-                {
-                    id: 3,
-                    name: "Quà sinh nhật",
-                    image: "https://www.hickoryfarms.com/dw/image/v2/AAOA_PRD/on/demandware.static/-/Sites-Web-Master-Catalog/default/dwff137641/images/products/lets-party-birthday-wine-gift-box-006437-1.jpg?sw=556&sh=680&sm=fit",
-                    original_price: 300000,
-                },
-            ],
-        };
-
+  name: "HomePage",
+  created() {
+    console.log("Home page created");
+    this.fetchHomeData();
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: "Quà năm mới",
+          image:
+            "https://www.angroos.com/wp-content/uploads/2024/12/realistic-New-Year-celebration-scene-featuring-a-luxurious-indoor.jpg",
+          original_price: 100000,
+        },
+        {
+          id: 2,
+          name: "Quà Valentine",
+          image:
+            "https://img.freepik.com/premium-photo/happy-valentine-day-gift-2023_781361-70.jpg",
+          original_price: 200000,
+        },
+        {
+          id: 3,
+          name: "Quà sinh nhật",
+          image:
+            "https://www.hickoryfarms.com/dw/image/v2/AAOA_PRD/on/demandware.static/-/Sites-Web-Master-Catalog/default/dwff137641/images/products/lets-party-birthday-wine-gift-box-006437-1.jpg?sw=556&sh=680&sm=fit",
+          original_price: 300000,
+        },
+      ],
+    };
+  },
+  methods: {
+    async fetchHomeData() {
+      try {
+        const response = await getHome();
+        if (response.status === 200) {
+          //   this.products = response.popularProducts;
+          console.log("ádasdasd");
+        }
+      } catch (error) {
+        console.error(error);
+      }
     },
-}
+  },
+};
 </script>
 
 <style scoped>
 .home-page {
-    height: 100vh;
-    padding: 50px;
+  height: 100vh;
+  padding: 50px;
 }
 
 .page-title {
-    text-align: center;
-    margin-bottom: 20px;
-    font-size: 24px;
-    color: #333;
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 24px;
+  color: #333;
 }
 
 .images {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 .images img {
-    width: 20%;
-    transition: transform 0.3s ease;
-    margin: 20px 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    border-radius: 10px;
+  width: 20%;
+  transition: transform 0.3s ease;
+  margin: 20px 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
 }
 
 .images img:hover {
-    transform: scale(1.1);
+  transform: scale(1.1);
 }
 
 .product-card {
-    width: 30%;
-    margin: 20px 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    border-radius: 10px;
-    overflow: hidden;
-    transition: transform 0.3s ease;
+  width: 30%;
+  margin: 20px 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  overflow: hidden;
+  transition: transform 0.3s ease;
 }
 
 .product-card img {
-    width: 100%;
-    border-bottom: 1px solid #ddd;
+  width: 100%;
+  border-bottom: 1px solid #ddd;
 }
 
 .product-info {
-    padding: 10px;
-    text-align: center;
+  padding: 10px;
+  text-align: center;
 }
 
 .product-info h3 {
-    margin: 10px 0;
-    font-size: 18px;
+  margin: 10px 0;
+  font-size: 18px;
 }
 
 .product-info p {
-    margin: 0;
-    color: #888;
+  margin: 0;
+  color: #888;
 }
 
 .product-card:hover {
-    transform: scale(1.05);
+  transform: scale(1.05);
 }
 </style>
